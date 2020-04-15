e8aff86f1c1948c34b28e759b5faa872e450d1cd
Fernando Ferrari <ferrarims85@gmail.com>
2020-04-15 16:14:07 +0100

    Service layer, component registration

---
+++ src/main/java/com/ferrari/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.ferrari.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ferrari.course.entites.User;
+import com.ferrari.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
